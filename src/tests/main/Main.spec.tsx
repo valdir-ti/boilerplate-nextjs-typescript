ef8383da98e8f68429aa31f19d2fc1728561c5d8
Valdir Silva <valdir.ti@gmail.com>
2023-02-20 02:17:52 -0300

    refact: move tests files to new folder

--- src/tests/main/Main.spec.tsx
+++ src/tests/main/Main.spec.tsx
@@ -1,7 +1,7 @@
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
 
-import Main from '.'
+import Main from '../../pages/components/Main'
 
 describe('<Main />', () => {
   it('should render a heading', () => {
