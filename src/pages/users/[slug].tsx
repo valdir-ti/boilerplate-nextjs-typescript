c2a14801e28dbbcc345d78074c68a822449662c3
Valdir Silva <valdir.ti@gmail.com>
2023-02-26 21:21:16 -0300

    feat: user id on dynamic router

--- src/pages/users/[slug].tsx
+++ src/pages/users/[slug].tsx
@@ -1,5 +1,10 @@
+import { useRouter } from 'next/router'
+
 export const User = () => {
-  return <div>User by param</div>
+  const router = useRouter()
+  const { slug } = router.query
+
+  return <div>User by param {slug}</div>
 }
 
 export default User
